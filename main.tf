dc217a29d0c0d8fcc1108a13ec3717c263646aa3
Enrique Pittaluga <ep7m@me.com>
2024-02-15 06:47:48 -0500

    fixing formatting

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-    backend "remote" {
+  backend "remote" {
     # The name of your Terraform Cloud organization.
     organization = "example-org-1bf7c7"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-        name = "example-workspace"
+      name = "example-workspace"
     }
-    }
+  }
 }
