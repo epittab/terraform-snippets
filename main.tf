c6a3ad1717b3beb0e373a711c122d13a39f315fd
Enrique Pittaluga <ep7m@me.com>
2024-02-15 07:59:25 -0500

    reverting

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    # organization = var.org
+    organization = "example-org-1bf7c7"
 
     # # The name of the Terraform Cloud workspace to store Terraform state files in.
-    # workspaces {
-    #   name = "example-workspace"
-    # }
+    workspaces {
+      name = "example-workspace"
+    }
   }
 }
