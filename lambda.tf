18dd1b434da102bdb64f2d053f60890eff05cb4b
Enrique Pittaluga <ep7m@me.com>
2024-02-17 17:07:27 -0500

    adding lambda infra

---
+++ lambda.tf
@@ -0,0 +1,17 @@
+data "archive_file" "init" {
+    type = "zip"
+    output_path = "${path.module}/lambda_function_payload.zip"
+
+    source {
+      content = "lambda"
+      filename = "init.txt"
+    }
+}
+
+resource "aws_lambda_function" "epb_lambda" {
+  filename = "${data.archive_file.init}"
+  function_name = "epb_lambda_name"
+  role = aws_iam_role.lambda_role.arn
+  runtime = "java17"
+  handler = "org.epb.test.Handler::handleRequest"
+}
