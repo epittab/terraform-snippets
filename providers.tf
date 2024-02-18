cd9f1c1e50d4d242bc53d6c1f617ecabcaac7a0b
Enrique Pittaluga <ep7m@me.com>
2024-02-18 10:49:36 -0500

    testing cloud parameter abstraction

--- providers.tf
+++ providers.tf
@@ -5,14 +5,14 @@ terraform {
       version = "~> 4.0"
     }
   }
-  backend "remote" {
+  cloud {
     # The name of your Terraform Cloud organization.
     organization = "example-org-1bf7c7"
 
     # # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "example-workspace"
-    }
+    # workspaces {
+    #   name = "example-workspace"
+    # }
   }
 
 
