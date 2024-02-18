49623bfc8fa0256161ed328e2c4ac37e934a0e60
Enrique Pittaluga <ep7m@me.com>
2024-02-18 11:03:19 -0500

    abstracting all vars

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
   cloud {
     # The name of your Terraform Cloud organization.
-    organization = "example-org-1bf7c7"
+    # organization = "example-org-1bf7c7"
 
     # # The name of the Terraform Cloud workspace to store Terraform state files in.
     # workspaces {
