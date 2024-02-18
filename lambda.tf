961b0b2b553447938336da203dface9885144900
Enrique Pittaluga <ep7m@me.com>
2024-02-18 16:25:01 -0500

    adding comment

--- lambda.tf
+++ lambda.tf
@@ -12,6 +12,6 @@ resource "aws_lambda_function" "epb_test_lambda" {
   filename      = data.archive_file.init.output_path
   function_name = "epb_lambda_name"
   role          = aws_iam_role.lambda_role.arn
-  runtime       = "java17"
-  handler       = "org.epb.test.Handler::handleRequest"
+  runtime       = "nodejs20.x"
+  handler       = "index.handler"
 }
