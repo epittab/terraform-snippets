d672548db83c7a85dccb81182f6420ebb5657718
Enrique Pittaluga <ep7m@me.com>
2024-02-18 11:41:17 -0500

    abstracting all vars

--- providers.tf
+++ providers.tf
@@ -5,15 +5,7 @@ terraform {
       version = "~> 4.0"
     }
   }
-  cloud {
-    # The name of your Terraform Cloud organization.
-    # organization = "example-org-1bf7c7"
-
-    # # The name of the Terraform Cloud workspace to store Terraform state files in.
-    # workspaces {
-    #   name = "example-workspace"
-    # }
-  }
+  cloud { }
 
 
 }
--- variables.tf
+++ variables.tf
@@ -7,12 +7,3 @@ variable "aws_region_az" {
   type    = string
   default = "us-east-1c"
 }
-
-# variable "TF_CLOUD_ORGANIZATION" {
-# }
-
-# variable "TF_CLOUD_HOSTNAME" {
-# }
-
-# variable "TF_WORKSPACE" {
-# }
