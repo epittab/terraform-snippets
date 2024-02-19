6d3d65baaca56ccb83ecb37997b1f9fa7b8e1d45
Enrique Pittaluga <ep7m@me.com>
2024-02-18 20:13:59 -0500

    fixing node version

--- lambda.tf
+++ lambda.tf
@@ -12,6 +12,6 @@ resource "aws_lambda_function" "epb_test_lambda" {
   filename      = data.archive_file.init.output_path
   function_name = "epb_lambda_name"
   role          = aws_iam_role.lambda_role.arn
-  runtime       = "nodejs20.x"
+  runtime       = "nodejs18.x"
   handler       = "index.handler"
 }
