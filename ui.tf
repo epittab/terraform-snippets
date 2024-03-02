a177aaf403ac14fcbe2b056e61962e19ac4ba0c4
Enrique Pittaluga <ep7m@me.com>
2024-03-01 19:23:10 -0500

    adding s3 static hosting bucket

---
+++ ui.tf
@@ -0,0 +1,40 @@
+
+resource "aws_s3_bucket" "ui" {
+  bucket = "ui-app-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "allow_access_to_ui" {
+  bucket = aws_s3_bucket.ui.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow_access_to_ui" {
+  bucket = aws_s3_bucket.ui.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [ "s3:GetObject" ]
+      Resource = ["arn:aws:s3:::ui-app-bucket/*"]
+      }]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+
+}
+
+
