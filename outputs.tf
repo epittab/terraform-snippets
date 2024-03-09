16f24c5d76f3015eb91b1d3bd6bc88130fc84274
Enrique Pittaluga <ep7m@me.com>
2024-03-09 17:21:07 -0500

    outputting corrected url

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,5 @@ output "domain" {
 }
 
 output "cognito" {
-  value = "https://${aws_cognito_user_pool.pool.domain}.auth.${var.aws_region}.amazoncognito.com/login?response_type=code&client_id=${aws_cognito_user_pool_client.userpool_client.id}&redirect_uri=${var.callback_url}"
+  value = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com/login?response_type=code&client_id=${aws_cognito_user_pool_client.userpool_client.id}&redirect_uri=${var.callback_url}"
 }
