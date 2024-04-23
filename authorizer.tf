9e583c5e5f55b3cafe19a57c5533e43ef5b4d153
Enrique Pittaluga <ep7m@me.com>
2024-04-22 21:36:16 -0400

    upgrading to node18

--- authorizer.tf
+++ authorizer.tf
@@ -69,5 +69,5 @@ resource "aws_lambda_function" "authorizer" {
 
   source_code_hash = data.archive_file.authorizer_lambda.output_base64sha256
 
-  runtime = "nodejs16.x"
+  runtime = "nodejs18.x"
 }
